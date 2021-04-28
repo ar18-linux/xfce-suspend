f9ede37170369216d65f3e7adfe8fb1e8ca2eaf1
nulysses <adr.zivil@gmail.com>
2021-04-28 22:11:33 +0200

    1

---
+++ suspend/suspend_no_password.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+function run() {
+  kill "$(pidof xfce4-power-manager-settings)" &
+  wait $!
+  xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/lock-screen-suspend-hibernate -s false &
+  wait $!
+  xfce4-power-manager -c &
+  wait $!
+  sleep 0.5
+  kill "$(pidof xfce4-power-manager-settings)" &
+  wait $!
+  systemctl suspend
+}
+
+
+run
