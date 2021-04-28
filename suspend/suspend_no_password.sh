558814f01a7704c4593bdba1c026e10c789e94ad
nulysses <adr.zivil@gmail.com>
2021-04-28 22:32:13 +0200

    1

--- suspend/suspend_no_password.sh
+++ suspend/suspend_no_password.sh
@@ -4,7 +4,7 @@
 function run() {
   kill "$(pidof xfce4-power-manager-settings)" &
   wait $!
-  xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/lock-screen-suspend-hibernate -s false &
+  xfconf-query -n -t bool -c xfce4-power-manager -p /xfce4-power-manager/lock-screen-suspend-hibernate -s false &
   wait $!
   xfce4-power-manager -c &
   wait $!
