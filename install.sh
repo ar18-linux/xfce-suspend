4eaa6278a23d1ed444f8a48dcf7f7b8bafbcc5ff
nulysses <adr.zivil@gmail.com>
2021-06-08 07:29:32 +0200

    1

--- install.sh
+++ install.sh
@@ -12,9 +12,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 . "${script_dir}/vars"
 
-if [ ! -d "${install_dir}" ]; then
-  mkdir -p "${install_dir}"
-fi
+mkdir -p "${install_dir}"
 
 rm -rf "${install_dir}/${module_name}"
 cp -rf "${script_dir}/${module_name}" "${install_dir}/${module_name}"
